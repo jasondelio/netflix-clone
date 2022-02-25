916dcfb7e108efc94237c672b9967ffb22fc3e9d
Jason <jason.delio@pegipegi.com>
2022-02-26 03:44:14 +1000

    update login design

--- src/login-page.js
+++ src/login-page.js
@@ -1,28 +1,39 @@
+import React, { useState } from "react";
 import "./login-page.css"
 import logo from './logo.svg';
 
 const LoginPage = () => {
+    const [userName, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handlerClicked = () => {
+        console.log(userName);
+        console.log(password);
+    }
+
     return (
-        <div class="login-wrapper">
-            <div class="login-background-wrapper">
-                <div class="background-overlay"></div>
+        <div className="login-wrapper">
+            <div className="login-background-wrapper">
+                <div className="background-overlay"></div>
             </div>
-            <img class="logo" src={logo} alt="logo"></img>
-            <div class="sign-in-wrapper">
-                <h1 class="sign-in-text">Sign in</h1>
-                <form>
-                    <ul class="no-bullets">
-                        <li>
-                            <input class="username-input" placeholder="Username or Email"></input>
-                        </li>
-                        <li>
-                            <input class="password-input" placeholder="Password" type="password"></input>
-                        </li>
-                        <li>
-                            <button class="sign-in-button">Sign in</button>
-                        </li>
-                    </ul>
-                </form>
+            <img className="logo" src={logo} alt="logo"></img>
+            <div className="sign-in-wrapper">
+                <h1 className="sign-in-text">Sign in</h1>
+                <ul className="no-bullets">
+                    <li>
+                        <input className="username-input" placeholder="Username or Email" onChange={(e) => setUsername(e.target.value)}></input>
+                    </li>
+                    <li>
+                        <input className="password-input" placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)}></input>
+                    </li>
+                    <li>
+                        <button className="sign-in-button" onClick={handlerClicked}>Sign in</button>
+                    </li>
+                    <li>
+                        <p className="sign-up-paragraph">New to netflix? <span className="sign-up-text">Sign up Now</span></p>
+                        
+                    </li>
+                </ul>
             </div>
         </div>
     );
